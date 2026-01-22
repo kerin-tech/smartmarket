c206abfc9bb43cbe1ab19852c6845f41a9892b5b
kerin-tech <melokerin1@gmail.com>
2026-01-22 15:50:04 -0500

    feat(backend): CRUD de tiendas funcional

--- apps/api/src/routes/index.ts
+++ apps/api/src/routes/index.ts
@@ -1,10 +1,14 @@
+// src/routes/index.ts
+
 import { Router } from 'express';
 import authRoutes from './auth.routes';
 import productRoutes from './product.routes';
+import storeRoutes from './store.routes';
 
 const router: Router = Router();
 
 router.use('/auth', authRoutes);
 router.use('/products', productRoutes);
+router.use('/stores', storeRoutes);
 
 export default router;
